f7cb07909746b82e4bbff3c625e4247a5a2ece5b
Shashank Rana <shashankr@xpanxion.co.in>
2018-01-03 14:44:40 +0530

    ShoppingList Commit

--- ShoppingKart/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ ShoppingKart/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Ingredient } from './../../shared/ingredient.model';
+import { Component, OnInit, ElementRef, ViewChild, EventEmitter, Output } from '@angular/core';
+
 
 @Component({
   selector: 'app-shopping-edit',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingEditComponent implements OnInit {
 
+  @ViewChild('ingName') nameInputRef:ElementRef;
+  @ViewChild('ingAmount') amountInputRef:ElementRef;
+  ingredient:Ingredient;
+  @Output() ingredientAdded =  new EventEmitter<Ingredient>();
   constructor() { }
 
   ngOnInit() {
+  }
+
+  addIngredient(){
+    this.ingredient = new Ingredient(this.nameInputRef.nativeElement.value, this.amountInputRef.nativeElement.value);
+    this.ingredientAdded.emit(this.ingredient);
   }
 
 }
