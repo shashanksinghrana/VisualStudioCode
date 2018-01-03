34d35ed113c1c6214243bf9e1217eb4c24aef918
shashanksinghrana <shankrana@gmail.com>
2018-01-03 09:26:30 +0530

    ComponentCommunication Home

--- ShoppingKart/src/app/recipes/recipes-list/recipes-list.component.ts
+++ ShoppingKart/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,5 +1,5 @@
 import { Recipe } from './../recipe.model';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 
 @Component({
@@ -9,12 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipesListComponent implements OnInit {
 
-  recipes: Recipe[] = [
+ recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'Test Description','http://www.seriouseats.com/images/2016/05/20160503-fava-carrot-ricotta-salad-recipe-1.jpg'),
-    new Recipe('A Test Recipe', 'Test Description','http://www.seriouseats.com/images/2016/05/20160503-fava-carrot-ricotta-salad-recipe-1.jpg')
+    new Recipe('Another Test Recipe', 'Test Description','https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg')
   ];
 
-  
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();  
   constructor() { 
     //console.log(this.recipes[0].name);
   }
@@ -22,5 +22,7 @@ export class RecipesListComponent implements OnInit {
   ngOnInit() {
    
   }
-
+  getRecipeDetail(recipe:Recipe){
+    this.recipeWasSelected.emit(recipe);
+  }
 }
